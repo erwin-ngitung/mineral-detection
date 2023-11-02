631c892c79de6b6db63a4c62e2d0b7ed67bffdca
Erwin Fernanda <fernandaerwin277@gmail.com>
2023-11-02 22:53:37 +0700

    Update app

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import logging
 
 st.set_page_config(
-    page_title="Home | C-MIND 1.0 App",
+    page_title="Home | MD 1.0 App",
     page_icon="🏠",
 )
 
@@ -22,12 +22,12 @@ st1, st2, st3 = st.columns(3)
 with st2:
     st.image(image)
 
-st.markdown('<h3 style=\'text-align:center;\'>Welcome to C-MIND 1.0! 👋</h3>', unsafe_allow_html=True)
+st.markdown('<h3 style=\'text-align:center;\'>Welcome to MD 1.0! 👋</h3>', unsafe_allow_html=True)
 
 st.markdown(
     """
     ### Want to learn more and purchase it?
-    - Check out [C-MIND 1.0 Website] (https://github.com/erwin-ngitung/c-mind-1.0)
+    - Check out [MD 1.0 Website] (https://github.com/erwin-ngitung/c-mind-1.0)
     - Jump into our [Documentation] (https://github.com/erwin-ngitung/c-mind-1.0)
     - Ask a question in our software (salsabilsudharta0724@gmail.com)
     """
